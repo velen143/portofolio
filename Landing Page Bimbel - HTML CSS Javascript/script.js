55f0c3d25b06bf28bdbb165a23c4065366d3644a
velen143 <velenshinta15@gmail.com>
2025-02-27 00:32:00 +0700

    Add files via upload

---
+++ Landing Page Bimbel - HTML CSS Javascript/script.js
@@ -0,0 +1,49 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    gragcursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    }
+  });
+
+  var swiper = new Swiper(".course-slider", {
+    loop:true,
+    centeredSlides: true,
+    autoplay:{
+      delay:9500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
