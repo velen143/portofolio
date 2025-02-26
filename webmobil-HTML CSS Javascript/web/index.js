08baaa881069a69e8c3f68353907087cc23588ce
velen143 <velenshinta15@gmail.com>
2025-02-27 00:28:48 +0700

    Add files via upload

---
+++ webmobil-HTML CSS Javascript/web/index.js
@@ -0,0 +1,65 @@
+const menuBar = document.querySelector(".menu-bar");
+const menuNav = document.querySelector(".menu");
+
+menuBar.addEventListener("click", () => {
+  menuNav.classList.toggle("menu-active");
+});
+
+const navBar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  const windowPosition = window.scrollY > 0;
+  navBar.classList.toggle("scrolling-active", windowPosition);
+  menuNav.classList.remove("menu-active");
+});
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    gragcursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    }
+  });
+
+  var swiper = new Swiper(".course-slider", {
+    loop:true,
+    centeredSlides: true,
+    autoplay:{
+      delay:9500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
